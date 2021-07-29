c100a829aeff4f2ea91a10099c58b7f17557eea6
Jlili <lili.jie@funplus.biz>
2021-07-29 16:21:28 +0800

    feat: 20210728 二叉树中所有距离为 K 的结点

--- 2021-07/28-all-nodes-distance-k-in-binary-tree.js
+++ 2021-07/28-all-nodes-distance-k-in-binary-tree.js
@@ -11,40 +11,42 @@
  * @param {number} k
  * @return {number[]}
  */
-var distanceK = function(root, target, k) {
-  let targetLevel = 0;
-  const level = [];
-  const cur = [root];
-  while (cur.length > 0) {
-    const len = cur.length;
-    level.push([...cur])
-    for (let i = 0; i < len; i++){
-      const node = cur.shift();
-      if (node === target) {
-        targetLevel = cur.length;
-      }
-      node.left && cur.push(node.left);
-      node.right && cur.push(node.right);
-    }
-  }
-  const res = [], upLevel = Math.abs(targetLevel - k), downLevel = targetLevel + k;
-
-  if (upLevel < level.length) {
-    for (let node of level[upLevel]) {
-      if (node !== target) {
-        res.push(node.val)
-      }
-    }
-  }
-
-  if (downLevel < level.length) {
-    for (let node of level[downLevel]) {
-      if (node !== target) {
-        res.push(node.val)
-      }
-    }
-  }
-
-  return res;
+var distanceK = function (root, target, k) {
+  link(root);
+  dfs(target);
 
 };
+
+let link = function (root) {
+  if (root.left) {
+    root.left.parent = root;
+    link(root.left)
+  }
+  if (root.right) {
+    root.right.parent = root;
+    link(root.right)
+  } 
+}
+
+let dfs = function (target, k) {
+  const res = [];
+  if (target.visited) {
+    return res;
+  }
+  if (k > 0) {
+    if (target.parent && !target.parent.visited) {
+      res = [...res,...dfs(target.parent, k - 1)]
+    }
+    if (target.left && !target.left.visited) {
+      res = [...res,...dfs(target.left, k - 1)]
+    }
+    if (target.right && !target.right.visited) {
+      res = [...res,...dfs(target.right, k - 1)]
+    }
+  }
+  if (k === 0) {
+    target.visited = true;
+    res.puah(target.val)
+  }
+  return res;
+}
