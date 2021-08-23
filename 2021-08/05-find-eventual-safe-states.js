7a449ba70f89e7b5df2e04c2cd0f8da4365d5658
Jlili <lili.jie@funplus.biz>
2021-08-23 17:10:16 +0800

    feat: 2021-08-05 优化

--- 2021-08/05-find-eventual-safe-states.js
+++ 2021-08/05-find-eventual-safe-states.js
@@ -5,33 +5,26 @@
 var eventualSafeNodes = function (graph) {
     const n = graph.length;
     const rg = new Array(n).fill(0).map(() => new Array());
-    const res = [];
-    let pre = 0;
+    const res = [],queue = []
     for (let x = 0; x < n; ++x) {
         for (let y of graph[x]) {
             rg[y].push(x);
         }
         if (graph[x].length === 0) {
-            res.push(x)
+            queue.push(x)
         }
     }
-    while (true) {
-        const len = res.length
-        for (let j = pre; j < len; j++){
-            const dist = res[j]
-            rg[dist].forEach(item => {
-                graph[item].splice(graph[item].indexOf(dist), 1)
-                if (graph[item].length === 0) {
-                    res.push(item)
-                }
-            })
-        }
-        if (res.length === len) {
-            break;
-        }
-        pre = len;
+    while (queue.length > 0) {
+        const dist = queue.shift()
+        res.push(dist)
+        rg[dist].forEach(item => {
+            graph[item].splice(graph[item].indexOf(dist), 1)
+            if (graph[item].length === 0) {
+                queue.push(item)
+            }
+        })
     }
     return res.sort((a, b) => a - b)
 };
 
-console.log(eventualSafeNodes([[1,2],[2,3],[5],[0],[5],[],[]]))
+console.log(eventualSafeNodes([[1,2,3,4],[1,2],[3,4],[0,4],[]]))
