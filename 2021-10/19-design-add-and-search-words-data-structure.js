24ecd4c0dae95075bebc0528ab20c275310a8a9a
Jlili <lili.jie@funplus.biz>
2021-10-26 16:10:32 +0800

    feat: 2021-10-19 添加与搜索单词

--- 2021-10/19-design-add-and-search-words-data-structure.js
+++ 2021-10/19-design-add-and-search-words-data-structure.js
@@ -1,20 +1,64 @@
 var WordDictionary = function () {
+  this.words = new Map()
 };
 
 /** 
  * @param {string} word
  * @return {void}
  */
-WordDictionary.prototype.addWord = function(word) {
-
+WordDictionary.prototype.addWord = function (word) {
+  const len = word.length;
+  let map = this.words
+  for (let i = 0; i < len; i++){
+    const letter = word[i]
+    if (!map.has(letter)) {
+      map.set(letter, {
+        value: new Map(),
+        tail: i === len - 1 ? true : false
+      })
+    } 
+    map = map.get(letter).value
+  }
 };
 
 /** 
  * @param {string} word
  * @return {boolean}
  */
-WordDictionary.prototype.search = function(word) {
-
+WordDictionary.prototype.search = function (word) {
+  const searchFromMap = function (word, map) {
+    const len = word.length;
+    for (let i = 0; i < len; i++){
+      const letter = word[i]
+      if (map.has(letter)) {
+        const node = map.get(letter)
+        map = node.value
+        if (node.tail && i === len -1) {
+          return true
+        }
+      } else if (letter === '.') {
+        const last = word.substring(i + 1)
+        if (i === len - 1) {
+          for (let node of map.values()) {
+            if (node.tail) {
+              return true
+            }
+          }
+        } else {
+          for (let node of map.values()) {
+            if (searchFromMap(last, node.value)) {
+              return true
+            }
+          }
+        }
+        return false
+      } else {
+        return false
+      }
+    }
+    return false;
+  }
+  return searchFromMap(word, this.words)
 };
 
 /**
@@ -23,3 +67,12 @@ WordDictionary.prototype.search = function(word) {
  * obj.addWord(word)
  * var param_2 = obj.search(word)
  */
+
+const ins = new WordDictionary()
+ins.addWord("bad");
+ins.addWord("dad");
+ins.addWord("mad");
+ins.search("pad"); // return False
+ins.search("bad"); // return True
+ins.search(".ad"); // return True
+ins.search("b.."); // return True
