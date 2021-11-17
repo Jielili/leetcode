47eb46e84d3348f751b3ed9f271e0e6a4ea22dfe
Jlili <jielilifz@163.com>
2021-11-17 19:43:17 +0800

    feat: 删除列表节点

---
+++ locf/shan-chu-lian-biao-de-jie-dian-lcof.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var deleteNode = function (head, val) {
+  if (head.val === val) {
+     return head.next
+  } else {
+    let p = head
+    while (p.next.val !== val) {
+      p=p.next
+    }
+    p.next = p.next.next
+    return head
+  }
+};
