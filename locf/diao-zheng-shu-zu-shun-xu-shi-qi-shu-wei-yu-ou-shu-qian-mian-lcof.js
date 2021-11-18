e653ae59488332a92e8749ea3c62b325da9b6afe
Jlili <jielilifz@163.com>
2021-11-18 17:57:08 +0800

    feat: 调整数组顺序使奇数位于偶数前面 双指针优化

--- locf/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof.js
+++ locf/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof.js
@@ -5,18 +5,25 @@
 var exchange = function (nums) {
   let i = 0, j = nums.length - 1
   while (i < j) {
-    if (nums[i] % 2 === 0) {
-      [nums[i], nums[j]] = [nums[j], nums[i]] 
-      j--
-    } else {
+    // if (nums[i] % 2 === 0) {
+    //   [nums[i], nums[j]] = [nums[j], nums[i]]
+    //   j--
+    // } else {
+    //   i++
+    // }
+    // if (nums[j] % 2 !== 0) {
+    //   [nums[i], nums[j]] = [nums[j], nums[i]]
+    //   i++
+    // } else {
+    //   j--
+    // }
+    while (i < j && nums[i] % 2 !== 0) {
       i++
     }
-    if (nums[j] % 2 !== 0) {
-      [nums[i], nums[j]] = [nums[j], nums[i]] 
-      i++
-    } else {
+    while (i < j && nums[j] % 2 === 0) {
       j--
     }
+    [nums[i], nums[j]] = [nums[j], nums[i]]
   }
   return nums
 };
