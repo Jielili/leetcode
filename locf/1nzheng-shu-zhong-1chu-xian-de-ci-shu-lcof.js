c778a3327d7ffbf6cc26c51305eddf152090c0af
Jlili <jielilifz@163.com>
2021-12-02 17:41:41 +0800

    feat: 优化下 循环  不用递归

--- locf/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof.js
+++ locf/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof.js
@@ -1,9 +1,9 @@
-                                            /**
+/**
  * @param {number} n
  * @return {number}
  */
-var countDigitOne = function (n) {
-  let d = 1,res = 0
+var countDigitOne1 = function (n) {
+  let d = 1, res = 0
   while (n >= d * 10) {
     res = res * 10 + d
     d *= 10
@@ -16,12 +16,12 @@ var countDigitOne = function (n) {
   let a = Math.floor(n / d)
   let b = n % d
   if (a == 1) {
-    res += b+1
-  } else if(a>1){
+    res += b + 1
+  } else if (a > 1) {
     res += d
-    res += (a-1)*pre
+    res += (a - 1) * pre
   }
-  
+
   if (b > 0) {
     res += countDigitOne(b)
   }
@@ -29,15 +29,36 @@ var countDigitOne = function (n) {
   return res
 };
 
+var countDigitOne = function (n) {
+  let d = 1, j = 0, res = 0, pre = 0;
+  while (n > 0) {
+    a = n % 10;
+    n = Math.floor(n / 10)
+    // res += j 
+    if (a == 1) {
+      res += j
+      res += pre + 1
+    } else if (a > 1) {
+      res += j
+      res += d
+      res += (a - 1) * j
+    }
+    pre = pre + a * d
+    j = j * 10 + d
+    d *= 10
+  }
+  return res
+};
+
 // 10，100，1000，10000，100000
 //  1， 20， 300， 4000， 50000
 
-console.log(countDigitOne(0))
 console.log(countDigitOne(1))
+console.log(countDigitOne(2))
 console.log(countDigitOne(4))
 console.log(countDigitOne(9))
 console.log(countDigitOne(10))
-console.log(countDigitOne(12))
+console.log(countDigitOne(13))
 console.log(countDigitOne(23))
 console.log(countDigitOne(99))
 console.log(countDigitOne(100))
