5a751368804cacba8e5f369877703feccc273cc9
Jlili <jielilifz@163.com>
2021-12-22 19:34:38 +0800

    feat: 平衡二叉树 DFS

---
+++ locf/ping-heng-er-cha-shu-lcof.js
@@ -0,0 +1,28 @@
+//  平衡二叉树
+//  输入一棵二叉树的根节点，判断该树是不是平衡二叉树。
+//  如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  if (!root) {
+    return true
+  }
+  return Math.abs(getDepth(root.left)-getDepth(root.right))<=1 && isBalanced(root.left) && isBalanced(root.right)
+};
+
+const getDepth = function (root) {
+  if (!root) {
+    return 0;
+  }
+  return Math.max(getDepth(root.left),getDepth(root.right))+1
+}
