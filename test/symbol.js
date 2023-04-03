524fca275f8536f601e77fe1557d343018e78f5f
Jlili <jielilifz@163.com>
2023-04-03 10:58:44 +0800

    feat: symbol

---
+++ test/symbol.js
@@ -0,0 +1,15 @@
+const a = Symbol()
+const b = null
+const c = undefined
+
+const d = {
+  5: 22332,
+  name: 'ateste'
+}
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(d.name)
+console.log(d[5])
+console.log(d['5'])
